3de14e8e2051bb04cd4ad16576659bc2baa75b81
AlexXi19 <alex2001314jjj@gmail.com>
2023-02-20 22:51:32 -0800

    chore: rust logging

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 use actix_web::{get, HttpResponse, Responder};
+use log::{info, LevelFilter};
+use swerve::logger::SimpleLogger;
+
+static LOGGER: SimpleLogger = SimpleLogger;
 
 #[actix_web::main]
 async fn main() {
+    log::set_logger(&LOGGER)
+        .map(|()| log::set_max_level(LevelFilter::Info))
+        .unwrap();
+
+    info!("Starting Swerve Web Server!");
     actix_web::HttpServer::new(|| actix_web::App::new().service(hello).service(ping))
         .bind(("127.0.0.1", 8080))
         .unwrap()
