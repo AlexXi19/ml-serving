d86873facbbfe0c2ec96d80091a53eb6022a982d
AlexXi19 <alex2001314jjj@gmail.com>
2023-02-20 22:40:07 -0800

    feat: init actix web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, HttpResponse, Responder};
+
+#[actix_web::main]
+async fn main() {
+    actix_web::HttpServer::new(|| actix_web::App::new().service(hello).service(ping))
+        .bind(("127.0.0.1", 8080))
+        .unwrap()
+        .run()
+        .await
+        .unwrap();
+}
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Welcome to Swerve!")
+}
+
+#[get("/ping")]
+async fn ping() -> impl Responder {
+    HttpResponse::Ok().body("pong")
 }
